2f8685decc7aaec3ef09dccdb5339a575b617b0d
muja07001 <muja07001@E-PW05CGR9>
2024-09-12 13:49:35 +0200

    Add project files.

---
+++ 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //   Ändra bakgrundsfärg till mörkröd
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+            //Ändra textfärgen till vit
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("Ett testmeddelande"); 
+
+        }
+    }
+}
